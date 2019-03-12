27b1feb34f7421d4b1bd8ecd8bc2298a255e8b0f
Luis Font <fontluis@gmail.com>
2019-03-12 12:42:35 -0600

    Initial Files

---
+++ FibTest.Console/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CodeTest.Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input: ");
+
+            var input = Console.ReadLine();
+            Console.WriteLine("\n");
+            if (int.TryParse(input, out var choice))
+            {
+               
+            }
+
+        }
+    }
+}
---
+++ FibTest.Contracts/ResultProvider.cs
@@ -0,0 +1,14 @@
+﻿namespace CodeTest.Contracts
+{
+    public abstract class ResultProvider<TInput,TResult>
+    {
+        public abstract string Name { get; }
+
+        protected abstract TResult DoGetResult(TInput input);
+
+        public TResult GetResult(TInput input)
+        {
+            return DoGetResult(input);
+        }
+    }
+}
